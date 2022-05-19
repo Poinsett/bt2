b27fc3c2492921a6445429f5d3d0caae361c4aaf
Poinsett <96115380+Poinsett@users.noreply.github.com>
2022-05-19 09:16:28 +0700

    ThanhTrang

---
+++ 3/app/src/main/java/com/example/a3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.a3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText bduonglich;
+    Button nhan;
+    TextView bamlich;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bduonglich = (EditText) findViewById(R.id.duonglich);
+        nhan= (Button) findViewById(R.id.nhan);
+        bamlich= (TextView) findViewById(R.id.amlich);
+        nhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String can[]= {"canh","Tân","Nhân","Quý","Giáp","Ất","Bình","Đinh","Mậu","Kỷ"};
+                String chi[]= {"Thân","Dậu","Tuất","Hợi","Tý","Sửu","Dần","Mẹo","Thìn","Tỵ","Ngọ","Mùi"};
+                String di = bduonglich.getText().toString();
+                int m= Integer.parseInt(di);
+                String am= can[m%10]+" "+chi[m%12];
+                bamlich.setText(am);
+            }
+        });
+    }
+
+}
